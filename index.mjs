0897e93618587dc484fa0ad3562da2cdd1912ec2
WWiRRu <wiiiruriiax@gmail.com>
2024-09-27 11:38:03 +0200

    test

--- index.mjs
+++
@@ -1,4 +0,0 @@
-import app from './src/app.mjs';
-
-const PORT = process.env.PORT || 80;
-app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
--- src/middlewares/error.middleware.mjs
+++
@@ -1,3 +0,0 @@
-import {StatusCodes} from "http-status-codes";
-
-export const errorMiddlewares = (error, _req, res, _next) => res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Something went wrong.', error: error.stack });
