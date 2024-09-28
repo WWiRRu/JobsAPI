655de6f6e3a35b38751fa1780d63fa8f62be0c1d
WWiRRu <wiiiruriiax@gmail.com>
2024-09-28 19:13:55 +0200

    ...

--- src/features/jobs/jobController.js
+++ src/features/jobs/jobController.js
@@ -1,5 +1,5 @@
-import {StatusCodes} from "http-status-codes";
+import { StatusCodes } from "http-status-codes";
 
 export const dashboard = async (req, res) => {
-
-}
+  res.status(StatusCodes.OK).json({message: "Welcome on the dashboard!"});
+};
--- src/features/jobs/jobsRoute.js
+++ src/features/jobs/jobsRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 import * as jobController from "./jobController.js";
 import {authMiddleware} from "../../middlewares/authMiddleware.js";
 
-const router = express.Router();-
+const router = express.Router();
 
 router.get("/dashboard", authMiddleware, jobController.dashboard);
 
